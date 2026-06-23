3fd5dee531cd4d3364233dd24a75fb1db8aa6c09
esteban-root <eromancastellanos@educa.madrid.org>
2026-06-23 18:02:06 +0000

    Añadidas las variables de entorno de la Clave, HOST y DEBUG

--- monitor/settings.py
+++ monitor/settings.py
@@ -9,27 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Entorno para las variables de entorno 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l0!gh#1)xfvo3i(34^hq@t!6d=%=isxkqepx+o&f314_kk3fvu'
-
+# SECRET_KEY = 'django-insecure-l0!gh#1)xfvo3i(34^hq@t!6d=%=isxkqepx+o&f314_kk3fvu'
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-     "monitor.corellafp.es",
-     "212.227.85.99",
-]
-
+# DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
+#ALLOWED_HOSTS = ["monitor.corellafp.es", "212.227.85.99",]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
